f2915fb8908ce2c0155351eda6c37332aa1f889a
danialbagheri <bagheri.danial@gmail.com>
2020-03-10 15:44:08 +0000

    upated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dpd_tracking_number_to_ebay_orders
 This python applications generates a  csv file with "Mark as dispatched template" of Ebay 2020
+
+
+## Step By step instructions
+
+1. download the csv file of all the "consignment" number with the "post code" of customers and save it as `dpd.csv`.
+2. Download the csv file of all the existing orders from ebay and save it as `orders.csv`.
+3. Software will find the post code and match all the ebay orders and will create the template `fileExchange.csv`.
+4. Upload the file in under selling > File Exchange > upload
---
+++ main.py
@@ -0,0 +1,78 @@
+import csv
+
+orderFile = open('orders.csv', newline='')
+dpdfile = open('dpd.csv', newline='')
+orders = csv.reader(orderFile)
+trackings = csv.reader(dpdfile)
+all_orders = []
+all_shipping = []
+
+for row in orders:
+    orders = {}
+    orders['post_code'] = row[18].replace(" ", "")
+    orders['item_number'] = row[20]
+    if len(row) < 45:
+        pass
+    else:
+        orders['transaction_id'] = row[45]
+    all_orders.append(orders)
+
+
+for i in trackings:
+    shipping = {}
+    shipping['consignment'] = i[2]
+    shipping['post_code'] = i[4]
+    all_shipping.append(shipping)
+
+
+def compare_lists(orders, shipping):
+    successful_count = 0
+    with open('fileExchange.csv', 'w', newline='') as fileExchange:
+        fieldnames = [
+            'Action',
+            'ItemID',
+            'TransactionID',
+            'ShippingStatus',
+            'ShippingCarrierUsed',
+            'ShipmentTrackingNumber',
+        ]
+        shippingwriter = csv.DictWriter(
+            fileExchange, fieldnames=fieldnames)
+        shippingwriter.writeheader()
+        for item in shipping:
+            for order in orders:
+                if order['post_code'] == item['post_code']:
+                    shippingwriter.writerow({
+                        'Action': "Status",
+                        'ItemID': order['item_number'],
+                        'TransactionID': order['transaction_id'],
+                        'ShippingStatus': '1',
+                        'ShippingCarrierUsed': 'DPD',
+                        'ShipmentTrackingNumber': item['consignment'],
+                    }
+                    )
+                    successful_count += 1
+    print(successful_count)
+
+
+compare_lists(all_orders, all_shipping)
+# import requests
+# url = "https://webservices.sandbox.ebay.com/BulkDataExchangeService"  #Sandbox
+# # url = "https://webservices.ebay.com/BulkDataExchangeService"  # Production
+# #headers = {'content-type': 'application/soap+xml'}
+# headers = {
+#     'content-type': 'text/xml',
+#     'X-EBAY-SOA-OPERATION-NAME': 'SetShipmentTrackingInfo',
+#     'X-EBAY-SOA-SECURITY-TOKEN':
+
+# }
+# body = """<?xml version="1.0" encoding="UTF-8"?>
+#          <SOAP-ENV:Envelope xmlns:ns0="http://ws.cdyne.com/WeatherWS/" xmlns:ns1="http://schemas.xmlsoap.org/soap/envelope/"
+#             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+#             <SOAP-ENV:Header/>
+#               <ns1:Body><ns0:GetWeatherInformation/></ns1:Body>
+#          </SOAP-ENV:Envelope>"""
+
+# response = requests.post(url,data=body,headers=headers)
+# print response.content
+
